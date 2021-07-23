e695c0580339cd762d711a459f9d8806a43a164b
Travis Hall <t.hall89d@gmail.com>
2021-07-23 16:34:14 -0400

    finish

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,16 +1,28 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js";
+const walkers = getWalkers();
+const pets = getPets();
 
-const pets = getPets()
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.id.startsWith("pet")) {
+    const [, petId] = itemClicked.id.split("--");
+    const petWalker = walkers.find((walker) => walker.id == petId);
+    for (const pet of pets) {
+      if (pet.id === parseInt(petId)) {
+        window.alert(`${pet.name} is being walked by ${petWalker.name}`);
+      }
+    }
+  }
+});
 
 export const RegisteredPets = () => {
-    let petHTML = "<ul>"
+  let petHTML = "<ul>";
 
-    for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
-    }
+  for (const pet of pets) {
+    petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`;
+  }
 
-    petHTML += "</ul>"
+  petHTML += "</ul>";
 
-    return petHTML
-}
-
+  return petHTML;
+};
